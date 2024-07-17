0a02a93d7291444592c53c04cef7eda8b7e521e0
Cryptorealm <uchennaanthony015@gmail.com>
2024-07-17 15:54:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+(function () {
+    const nav = document.querySelector('.navigation');
+    const contact = document.querySelector('.second-btn');
+    const resume = document.querySelector('.btn');
+
+
+    resume.addEventListener('btn')
+})
